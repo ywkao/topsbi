d962e541033155a4824db29b27b79a4b33dcbcde
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2026-07-08 23:53:11 +0200

    set up wandb for condor jobs

--- condor/run.sh
+++ condor/run.sh
@@ -7,4 +7,10 @@ nvidia-smi -L || true
 
 source /cvmfs/sft.cern.ch/lcg/views/LCG_109_cuda/x86_64-el9-gcc13-opt/setup.sh
 
+# pick up user-installed packages (e.g. wandb) not present in the LCG view
+export PYTHONPATH=/eos/user/y/ykao/.local/lib/python3.13/site-packages:$PYTHONPATH
+
+# uncomment if worker nodes can't reach api.wandb.ai; sync later with `wandb sync <run-dir>`
+# export WANDB_MODE=offline
+
 python /eos/home-y/ykao/SWAN_projects/analysis/topsbi/condor/train.py "$CONFIG"
